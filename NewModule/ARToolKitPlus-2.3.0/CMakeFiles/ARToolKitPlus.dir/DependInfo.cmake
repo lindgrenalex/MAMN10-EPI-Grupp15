8b90e5dd2d9d36d784d6466e3da9902f3d5b4c52
lindgrenalex <alex.lindgren93@gmail.com>
2016-09-08 15:46:45 +0200

    First push NewModule

---
+++ NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/Camera.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/Camera.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/Tracker.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/Tracker.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/TrackerMultiMarker.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/TrackerMultiMarker.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/TrackerSingleMarker.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/TrackerSingleMarker.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arBitFieldPattern.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arBitFieldPattern.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arDetectMarker.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arDetectMarker.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arDetectMarker2.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arDetectMarker2.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arGetCode.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arGetCode.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arGetInitRot2.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arGetInitRot2.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arGetMarkerInfo.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arGetMarkerInfo.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arGetTransMat.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arGetTransMat.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arGetTransMat2.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arGetTransMat2.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arGetTransMat3.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arGetTransMat3.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arGetTransMatCont.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arGetTransMatCont.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arLabeling.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arLabeling.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arMultiActivate.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arMultiActivate.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arMultiGetTransMat.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arMultiGetTransMat.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arMultiGetTransMatHull.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arMultiGetTransMatHull.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arMultiReadConfigFile.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arMultiReadConfigFile.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/arUtil.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/arUtil.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/mPCA.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/mPCA.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/matrix.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/matrix.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/paramDecomp.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/paramDecomp.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/paramDistortion.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/paramDistortion.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/rppGetTransMat.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/rppGetTransMat.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/rppMultiGetTransMat.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/rppMultiGetTransMat.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/core/vector.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/core/vector.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/extra/BCH.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/extra/BCH.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/extra/Hull.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/extra/Hull.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/librpp/arGetInitRot2Sub.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/librpp/arGetInitRot2Sub.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/librpp/librpp.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/librpp/librpp.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/librpp/rpp.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/librpp/rpp.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/librpp/rpp_quintic.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/librpp/rpp_quintic.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/librpp/rpp_svd.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/librpp/rpp_svd.cpp.o"
+  "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/src/librpp/rpp_vecmat.cpp" "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/CMakeFiles/ARToolKitPlus.dir/src/librpp/rpp_vecmat.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ NewModule/ARToolKitPlus-2.3.0/cmake_install.cmake
@@ -0,0 +1,56 @@
+# Install script for directory: /users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE SHARED_LIBRARY FILES "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/lib/libARToolKitPlus.dylib")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libARToolKitPlus.dylib" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libARToolKitPlus.dylib")
+    execute_process(COMMAND "/usr/bin/install_name_tool"
+      -id "libARToolKitPlus.dylib"
+      "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libARToolKitPlus.dylib")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/libARToolKitPlus.dylib")
+    endif()
+  endif()
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE DIRECTORY FILES "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/include/")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/users/alexanderlindgren/ikaros/Source/NewModule/ARToolKitPlus-2.3.0/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
